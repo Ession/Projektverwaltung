bc654eef208cfbd5a68c618f38eeab8a3dbe6a47
dark <mail@mathiasjost.com>
2012-12-18 21:53:32 +0100

    initial project files and main class and packages

---
+++ src/de/fhswf/classes/Projektverwaltung.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fhswf.classes;
+
+/**
+ *
+ * @author Dark
+ */
+public class Projektverwaltung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
